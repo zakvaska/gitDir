7bc405e4dc0ed701aa19c2eaa633f855db9e57f4
zakvaska <61754881+zakvaska@users.noreply.github.com>
2020-09-23 12:11:18 +0300

    Add files via upload

---
+++ JavaScript/Advanced/homework/hw1/JS/hw1v2.js
@@ -0,0 +1,77 @@
+var choosenIndex = 0;
+var unorderedList = document.getElementById('list');
+var htmlNodes = unorderedList.children;
+
+function chooseElement(index) { //выбор элемента с указанным индексом
+    if (htmlNodes.length > 0) {
+        var choosenListItem = htmlNodes[index];
+        choosenListItem.style.color = 'green';
+        choosenListItem.style.backgroundColor = 'yellow';
+        // console.log(`index = ${index}`);
+    }
+};
+function unchooseElement(index) { //снятие выделения для перемещения выбора
+    var choosenListItem = htmlNodes[index];
+    choosenListItem.removeAttribute('style');
+};
+function chooseFirst() { //выбор первого элемента
+    console.log(`choosenIndex = ${choosenIndex}`);
+    unchooseElement(choosenIndex);
+    choosenIndex = 0;
+    chooseElement(choosenIndex);
+};
+function chooseLast() { //выбор последнего элемента
+    unchooseElement(choosenIndex);
+    var lastIndex = htmlNodes.length - 1;
+    choosenIndex = lastIndex;
+    chooseElement(choosenIndex);
+};
+function goPrev() { //выбор следующего элемента
+    unchooseElement(choosenIndex);
+    choosenIndex--;
+    if (choosenIndex < 0) {
+        choosenIndex = htmlNodes.length - 1;
+    };
+    chooseElement(choosenIndex);
+};
+function goNext() { //выбор предыдущего элемента
+    unchooseElement(choosenIndex);
+    choosenIndex++;
+    if (choosenIndex > htmlNodes.length - 1) {
+        choosenIndex = 0;
+    };
+    chooseElement(choosenIndex);
+};
+function addToBegin(form) { //добавление нового элемента в начало списка
+    if (form.input.value) {
+        var newListItem = document.createElement('li');
+        newListItem.innerHTML = form.input.value;
+        var firstListItem = htmlNodes[0];
+        unorderedList.insertBefore(newListItem, firstListItem);
+        choosenIndex++; /*Добавление нового элемента в начало списка сдвигает индекс всех остальных элементов на 1*/
+    };
+};
+function addToEnd(form) { //добавление нового элемента в конец списка
+    if (form.input.value) {
+        var newListItem = document.createElement('li');
+        newListItem.innerHTML = form.input.value;
+        unorderedList.appendChild(newListItem);
+    };
+};
+function deleteElement() {
+    var itemToDelete = htmlNodes[choosenIndex];
+    unorderedList.removeChild(itemToDelete);
+    // console.log(`choosenIndex = ${choosenIndex}`);
+    // console.log(`htmlNodes.length = ${htmlNodes.length}`);    
+    if (choosenIndex == htmlNodes.length) { //Например из массива с длинной 5 и индексом последнего элемента 4 сразу после удаления соответствующего
+        //html-узла из DOM-дерева удаляется последний элемент. Обновляется массив, длинна теперь на 1 меньше, то есть 4.
+        //В choosenIndex все еще старый индекс. Он сравнивается с новой длинной массива
+        //Если они совпадают - значит удаленный элемент был последним в списке и нужно выделить новый последний элемент
+        chooseElement(--choosenIndex); //выделяем новый последний элемент после удаления последнего 
+    } else {
+        chooseElement(choosenIndex);
+    }
+    // console.log(`choosenIndex = ${choosenIndex}`);
+};
+
+
